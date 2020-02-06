994a6273ff75139d39d1f73512dc83cfccb37084
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-02-06 14:52:43 +0530

    /Akshay

--- src/main/java/com/ats/rusafrontend/commen/XssEscapeUtils.java
+++ src/main/java/com/ats/rusafrontend/commen/XssEscapeUtils.java
@@ -24,49 +24,51 @@ public class XssEscapeUtils {
 	// a) in all input type text and textarea without ckeditor or any html editor
 	// in both project
 	public static String jsoupParse(String str) {
-		str = str.replaceAll("\\<.*?\\>", "");
-		str=Jsoup.parse(str).text();
-		//System.err.println("str1 in jsoupParse " +str);
-		//str = URLEncoder.encode(str);
+
 		try {
-			str =URLDecoder.decode(str);
-		}catch(Exception e) {
-			
-		}
-		
-		
-	  	str=StringEscapeUtils.escapeHtml4(str);
-	 	str=StringEscapeUtils.escapeJava(str);
-		//str= StringEscapeUtils.escapeEcmaScript(str);
-		//str=StringEscapeUtils.unescapeHtml4(str);
-		 
-	//	str=StringEscapeUtils.escapeJava(str); 
-		//System.err.println("str1 in encode " +str);
-		return str;
-		}
+			str = str.replaceAll("\\<.*?\\>", "");
+			str = Jsoup.parse(str).text();
+			// System.err.println("str1 in jsoupParse " +str);
+			// str = URLEncoder.encode(str);
+			try {
+				str = URLDecoder.decode(str);
+			} catch (Exception e) {
 
-		// b) in all textarea with ckeditor or any html editor in both project
-		public static String jsoupParseClean(String str) {
-			//Jsoup.clean(str, Whitelist.relaxed())
-		return str;
-		}
+			}
 
-		public static String jsoupParseOutput(String str) {
-		//System.err.println("str0 " +str);
+			str = StringEscapeUtils.escapeHtml4(str);
+			str = StringEscapeUtils.escapeJava(str);
+			// str= StringEscapeUtils.escapeEcmaScript(str);
+			// str=StringEscapeUtils.unescapeHtml4(str);
+
+			// str=StringEscapeUtils.escapeJava(str);
+			// System.err.println("str1 in encode " +str);
+		} catch (Exception e) {
+
+		}
+		return str;
+	}
+
+	// b) in all textarea with ckeditor or any html editor in both project
+	public static String jsoupParseClean(String str) {
+		// Jsoup.clean(str, Whitelist.relaxed())
+		return str;
+	}
+
+	public static String jsoupParseOutput(String str) {
+		// System.err.println("str0 " +str);
 
 //			str = URLEncoder.encode(str);
 //			System.err.println("str1 encode " +str);
 
+		str = StringEscapeUtils.unescapeHtml4(str);
+		// System.err.println("str2 unescapeHtml4 " +str);
 
+		str = StringEscapeUtils.escapeJava(str);
 
-		str=StringEscapeUtils.unescapeHtml4(str);
-		//System.err.println("str2  unescapeHtml4 " +str);
-
-		str=StringEscapeUtils.escapeJava(str);
-
-		//System.err.println("str3 unescapeJava " +str);
+		// System.err.println("str3 unescapeJava " +str);
 		return str;
-		}
+	}
 	/*
 	 * public static String jsoupParse(String str) { return Jsoup.parse(str).text();
 	 * }
